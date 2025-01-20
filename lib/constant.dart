b9ae2c9c187e280c4884484c8508d39d10a4f4e5
AthulKrishna2000 <athul997788@gmail.com>
2025-01-20 16:27:23 +0530

    change api

--- lib/constant.dart
+++ lib/constant.dart
@@ -1,3 +1,2 @@
-const String domain =
-    'https://api.openweathermap.org/data/2.5/weather?';
-const String apikey = '8aaf76681d0e4252855f1f3868b1e00c';
+const String domain = 'https://api.openweathermap.org/data/2.5/weather?';
+const String apikey = '';
